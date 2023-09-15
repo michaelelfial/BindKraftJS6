2ca11a41f6584b878353ef35f3ef5811cfbec191
Robert Tanev <rtanev@cleancodefactory.de>
2023-09-15 15:17:18 +0300

    fix css compilation

--- CssCompile.bat
+++ CssCompile.bat
@@ -1,10 +1,10 @@
-SET ThemeName=%1
-
+ECHO OFF
 SET CurrentDir=%~dp0
 
 if not exist %CurrentDir%Css mkdir %CurrentDir%Css
 ECHO //#using "./%ThemeName%.css" > %CurrentDir%Css/%ThemeName%.dep
-call sass %CurrentDir%Css/BindKraftJS6.scss %CurrentDir%Css/%ThemeName%.css %2 %3 %4
+
+kraft system -scssfile %CurrentDir%Css/BindKraftJS6.scss -cssfile %CurrentDir%Css/%ThemeName%.css -themename %ThemeName% %Optimize% %Noninteractive%
 
 ECHO BindKraftJS6 Build Successfully
 
